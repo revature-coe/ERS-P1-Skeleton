aa796a7b4a353852cdd0be1e4cd1d0ce7d58d79a
IkenoXamos <moberlies1337@gmail.com>
2021-11-29 15:21:34 -0500

    Removed unused imports

--- src/test/java/com/revature/util/ConnectionFactoryTest.java
+++ src/test/java/com/revature/util/ConnectionFactoryTest.java
@@ -1,21 +1,13 @@
 package com.revature.util;
 
-import com.revature.models.Role;
-import com.revature.models.User;
-import com.revature.repositories.UserDAO;
-import com.revature.services.UserService;
-import org.junit.Before;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.Connection;
-import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.*;
 
 public class ConnectionFactoryTest {
 
